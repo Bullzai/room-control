42b4e5d0685ac7f9c958282ce550c643fc1c1ef5
Vidmantas <valskisv@gmail.com>
2022-12-20 22:47:05 +0000

    pi script code clean up

--- pi_scripts/humid-sensor.py
+++ pi_scripts/humid-sensor.py
@@ -4,54 +4,52 @@ from datetime import datetime, timedelta
 from sense_hat import SenseHat
 from picamera import PiCamera
 
+frequency = 10
 camera = PiCamera()
 sense = SenseHat()
 sense.clear()
-frame = 1
-green = (0, 180, 0)
-red = (255, 0, 0)
-color = (200, 0, 200)
 # set location of image file and current time
-picLoc = f'/home/pi/photos/frame{frame}.jpg'
+picLoc = f'/home/pi/photos/frame1.jpg'
+# set url of image file to upload
 uploadUrl = "http://vimo.lt:25998/upload"
 files = {'image': open('/home/pi/photos/frame1.jpg', 'rb')}
+# set your own host IP or domain address
 host = "vimo.lt"
-port = 25999                   # The same port as used by the server
+# set the same port as used by your TCP server
+port = 25999
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 s.sendall(b'Its a me, Mario')
-# print('Received', repr(data))
 date = datetime.now()
 date2 = datetime.now()
 
 
 def photo():
-    print('darom fotke')
+    print('taking a photo')
     camera.capture(picLoc)
     # upload the photo to your web server
-    print('uploadinam fotke')
+    print('uploading the photo')
     files = {'image': open('/home/pi/photos/frame1.jpg', 'rb')}
     r = requests.post(uploadUrl, files=files)
+    serverMsg = r.text
+    if serverMsg == 'done':
+        print(serverMsg)
+        s.sendall(bytes("new_photo", "utf-8"))
 
 
+# check on TCP server every second if there's any requests & send humidity readings every 10 secods.
 while True:
     now = datetime.now()
     if now > date2 + timedelta(seconds=1):
         s.sendall(bytes("0", "utf-8"))
-        sense.show_message("O", scroll_speed=0.06, text_colour=red)
         data = s.recv(1024).decode()
         if int(data) > 0:
-            # currentTime = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-            # picLoc = f'/home/pi/photos/frame{frame}.jpg'
             photo()
-            frame += 1
         date2 = now
-    if now > date + timedelta(seconds=10):
+    if now > date + timedelta(seconds=frequency):
         humid = sense.get_humidity()
         if humid >= 25:
             text = str(humid)
             s.sendall(bytes(text, "utf-8"))
-        else:
-            sense.show_message("vava", text_colour=green)
         print(humid)
         date = now
